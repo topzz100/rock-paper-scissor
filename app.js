fec951d82a90a46dbbe44cb8cecd4dee78ef05f4
topzz100 <adenekantope@gmail.com>
2021-09-27 16:00:49 +0100

    update

---
+++ app.js
@@ -0,0 +1,15 @@
+const gameStart = document.querySelector('.game-on');
+const intro = document.querySelector('.intro');
+const startGame = document.querySelector('.start-game');
+const options = document.querySelectorAll('.option');
+const playerScore = document.querySelector('.p-score-value')
+const computerScore = document.querySelector('.c-score-value')
+const player = document.querySelector('.player')
+const computer = document.querySelector('.computer')
+const notification = document.querySelector('.notification')
+
+let pScore = 0;
+let cScore = 0;
+
+// playerScore.textContent = pScore;
+// computerscore.textContent = cScore;
