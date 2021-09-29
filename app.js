a1274935cb223d8c31a3bf2edf83a7038f22665a
topzz100 <adenekantope@gmail.com>
2021-09-29 14:32:07 +0100

    a little update

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
+
 const gameStart = document.querySelector('.game-on');
 const intro = document.querySelector('.intro');
 const startGame = document.querySelector('.start-game');
 const options = document.querySelectorAll('.option');
-const playerScore = document.querySelector('.p-score-value')
-const computerScore = document.querySelector('.c-score-value')
-const player = document.querySelector('.player')
-const computer = document.querySelector('.computer')
-const notification = document.querySelector('.notification')
+const playerScore = document.querySelector('.p-score-value');
+const computerScore = document.querySelector('.c-score-value');
+const player = document.querySelector('.player');
+const computer = document.querySelector('.computer');
+const notification = document.querySelector('.notification');
 
 let pScore = 0;
 let cScore = 0;
 
+
+//computer hand options
 const computerOptions = ['rock', 'paper', 'scissors'];
 
-
+//click event - choosing option
 options.forEach(option => {
   option.addEventListener('click', () => {
     gameAnimation();
+
+    //setting timeout to the duration of the game animation
      setTimeout(() => {
       const choice = computerChoice();
     
       player.src = `./images/${option.textContent}.png`;
       computer.src = `./images/${choice}.png`;
   
-      checkWinner(option.textContent, choice)
-      console.log(choice)
-      console.log(option.textContent)
-      removeAnimation()
-     }, 1000)
+      checkWinner(option.textContent, choice);
+      removeAnimation();
+     }, 1000);
    
-  })
+  });
 });
 
+//hand animation
 const gameAnimation = () => {
   player.src = `./images/rock.png`;
   computer.src = `./images/rock.png`;
   player.style.animation = 'player-animation 1s ease'
   computer.style.animation = `computer-animation 1s ease`
-}
+};
 
 const removeAnimation = () => {
   player.style.animation = 'none'
@@ -50,13 +54,14 @@ const computerChoice = () => {
   return compChoice = computerOptions[compIndex]
 };
 
+// change to the game display
 startGame.addEventListener('click', () => {
   intro.classList.add('hide')
   gameStart.classList.add('show')
 
 });
 
-
+//logic to know winner and add score
 const checkWinner = (pChoice, cChoice) => {
   if(pChoice === cChoice){
     notification.textContent = `it's a tie`
@@ -71,8 +76,7 @@ const checkWinner = (pChoice, cChoice) => {
         pScore++
         playerScore.textContent = pScore;
       }
-      // notification.textContent = `player wins`
-      // return
+    
     }else if(pChoice === 'paper'){
       if(cChoice === 'rock'){
         notification.textContent = `player wins`
